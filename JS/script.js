75dde7e8be2c9d6be177f2a7e37d806cbebf5f2e
Kolya Kovalenko <155112540+NicoKovalenko@users.noreply.github.com>
2024-07-29 18:17:41 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+// let name = "Іван"; //Іван
+// let city = "Kyiv";
+// city = name;
+// console.log(city);
+
+// let name = "Ольга";
+// console.log(`привіт ${1}`); //привіт Ольга
+// console.log(`привіт ${"name"}`);
+// console.log(`привіт ${name}`);
+
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(typeof Number(a));
+// console.log(typeof parseInt(b));
+// console.log(typeof parseFloat(c)); //number
+
+// console.log((0.1*10 + 0.2*10)/10); //0.3
+
+// console.log(Math.max(20,10,50,40)); //50
+
+// console.log(Math.random()*(4-2)+2); //3.3756253620610583
+
+// const message = "Welcome to Bahamas!"; //WELCOME TO BAHAMAS1
+// console.log(message.length); //19
+
+// console.log(message.toUpperCase());  //WELCOME TO BAHAMAS1
+
+// let user = {};
+// user.name = "Nico"; //{name: 'Nico', age: 30, city: 'Kyiv'}
+// user.age = 30;
+// user.city = "Kyiv";
+// // console.log(user);
+// delete user.city;
+// user['like flowers'] = true; //{name: 'Nico', age: 30, like flowers: true}
+// // console.log(user);
+
+// for (key in user){
+//    console.log(key);
+//    console.log(user[key]); 
+// }    
+//script.js:39 name
+//script.js:40 Nico
+//script.js:39 age
+//script.js:40 30
+//script.js:39 like flowers
+// script.js:40
+
